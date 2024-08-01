8f893739baf4adb6ba82a6d72c751f2ae8193bd5
Noorrizky <noorrizkypermana@gmail.com>
2024-08-01 15:22:12 +0800

    Db updated

--- db/digitalent.sql
+++ db/digitalent.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 28, 2024 at 08:55 AM
+-- Generation Time: Aug 01, 2024 at 09:21 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -20,6 +20,29 @@ SET time_zone = "+00:00";
 --
 -- Database: `digitalent`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pariwisata`
+--
+
+CREATE TABLE `pariwisata` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `link_info` varchar(255) NOT NULL,
+  `link_video` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pariwisata`
+--
+
+INSERT INTO `pariwisata` (`id`, `nama`, `deskripsi`, `gambar`, `link_info`, `link_video`) VALUES
+(1, 'Loksado', 'Nikmati keindahan alam Kalimantan Selatan yang memukau.', 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/07/a3/93/ed/loksado-bamboo-rafting.jpg?w=1000&h=800&s=1', 'https://www.indonesia.travel/id/id/home', 'https://www.youtube.com/watch?v=gZCCwxMC6AU&pp=ygUXa3Jpc3RpYW4gaGFuc2VuIGxva3NhZG8%3D'),
+(2, 'Riam Kanan', 'Jelajahi kekayaan budaya dan alam Kalimantan Selatan.', 'https://wisato.id/wp-content/uploads/2020/03/26305800_197798214132150_237867301876006912_n-1068x801.jpg', 'https://www.indonesia.travel/id/id/search.html?q=Kalimantan+Selatan', 'https://www.youtube.com/watch?v=4R8VJI66ryc&pp=ygUXa3Jpc3RpYW4gaGFuc2VuIGxva3NhZG8%3D');
 
 -- --------------------------------------------------------
 
@@ -55,6 +78,12 @@ INSERT INTO `user` (`username`, `id_user`, `password`, `role`) VALUES
 --
 
 --
+-- Indexes for table `pariwisata`
+--
+ALTER TABLE `pariwisata`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -63,6 +92,12 @@ ALTER TABLE `user`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `pariwisata`
+--
+ALTER TABLE `pariwisata`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user`
